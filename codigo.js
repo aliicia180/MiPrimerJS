64238dd65334fcb4282b508dd201218791e2de47
aliicia180 <148622742+aliicia180@users.noreply.github.com>
2023-11-04 19:21:59 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,3 +1,12 @@
 const miTitulo = document.querySelector("h1");
 miTitulo.textContent = "¡Hola mundo!";
+  let nombre ="Alicia";
+    const edad = 19;
 
+    document.write(`Mi nombre es ${nombre} y tengo ${edad} años.`);
+	
+let perro = "bimba";
+if (perro === "bimba") {
+  alert("Mi perra bimba es la mejor");
+} else {
+  alert("Bimba es el nombre de mi perra");
